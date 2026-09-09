04aeb0d18f6825485cf455ed37ec84d6de42c556
yamat0jp <yamat0jp@yahoo.co.jp>
2026-09-09 19:59:20 +0900

    Initial commit

---
+++ Unit1.dfm
@@ -0,0 +1,75 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  AutoSize = True
+  BorderStyle = bsToolWindow
+  Caption = 'GifCapture'
+  ClientHeight = 25
+  ClientWidth = 264
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -12
+  Font.Name = 'Segoe UI'
+  Font.Style = []
+  OnCloseQuery = FormCloseQuery
+  OnCreate = FormCreate
+  OnDestroy = FormDestroy
+  TextHeight = 15
+  object ActionToolBar1: TActionToolBar
+    Left = 0
+    Top = 0
+    Width = 264
+    Height = 25
+    ActionManager = ActionManager1
+    Caption = 'ActionToolBar1'
+    Color = clMenuBar
+    ColorMap.DisabledFontColor = 10461087
+    ColorMap.HighlightColor = clWhite
+    ColorMap.BtnSelectedFont = clBlack
+    ColorMap.UnusedColor = clWhite
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clBlack
+    Font.Height = -12
+    Font.Name = 'Segoe UI'
+    Font.Style = []
+    ParentFont = False
+    Spacing = 0
+    ExplicitWidth = 624
+  end
+  object ActionManager1: TActionManager
+    ActionBars = <
+      item
+        Items = <
+          item
+            Action = Action1
+          end
+          item
+            Caption = '-'
+          end
+          item
+            Action = Action2
+            Caption = #32066#20102'(&Y)'
+          end>
+        ActionBar = ActionToolBar1
+      end>
+    Left = 336
+    Top = 72
+    StyleName = 'Platform Default'
+    object Action1: TAction
+      Caption = #23455#34892
+      OnExecute = Action1Execute
+    end
+    object Action2: TAction
+      Caption = #32066#20102
+      OnExecute = Action2Execute
+    end
+  end
+  object Timer1: TTimer
+    Enabled = False
+    Interval = 500
+    OnTimer = Timer1Timer
+    Left = 240
+    Top = 72
+  end
+end
---
+++ Unit1.pas
@@ -0,0 +1,174 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, System.Actions, Vcl.ActnList,
+  Vcl.ToolWin, Vcl.ActnMan, Vcl.ActnCtrls, Vcl.PlatformDefaultStyleActnCtrls,
+  Vcl.ExtCtrls, System.Generics.Collections;
+
+type
+  TForm1 = class(TForm)
+    ActionManager1: TActionManager;
+    ActionToolBar1: TActionToolBar;
+    Action1: TAction;
+    Timer1: TTimer;
+    Action2: TAction;
+    procedure FormDestroy(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure Action1Execute(Sender: TObject);
+    procedure Action2Execute(Sender: TObject);
+    procedure FormCloseQuery(Sender: TObject; var CanClose: Boolean);
+    procedure Timer1Timer(Sender: TObject);
+  private
+    { Private 宣言 }
+    List: TObjectList<TBitmap>;
+  public
+    { Public 宣言 }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+uses Vcl.Imaging.GIFImg, System.Threading;
+
+const
+  title = 'GIF Capture %s';
+
+var
+  task: ITask;
+
+{$R *.dfm}
+
+// 画面全体をキャプチャしてTBitmapに返す関数
+function CaptureScreen: TBitmap;
+var
+  bmp: TBitmap;
+  DesktopCanvas: TCanvas;
+  DC: HDC;
+begin
+  Result := TBitmap.Create;
+  try
+    // 画面の解像度を取得
+    Result.Width := Screen.Width;
+    Result.Height := Screen.Height;
+    Result.PixelFormat := pf16bit;
+
+    // デスクトップのデバイスコンテキスト(DC)を取得
+    DC := GetDC(0);
+    try
+      DesktopCanvas := TCanvas.Create;
+      try
+        DesktopCanvas.Handle := DC;
+        // BitBltで画面のピクセルデータをTBitmapに高速コピー
+        BitBlt(Result.Canvas.Handle, 0, 0, Result.Width, Result.Height,
+               DesktopCanvas.Handle, 0, 0, SRCCOPY);
+      finally
+        DesktopCanvas.Free;
+      end;
+    finally
+      ReleaseDC(0, DC);
+    end;
+    bmp:=TBitmap.Create(Result.Width div 2, Result.Height div 2);
+    try
+      bmp.Canvas.StretchDraw(TRect.Create(0,0,bmp.Width,bmp.Height),Result);
+      Result.Assign(bmp);
+    finally
+      bmp.Free;
+    end;
+  except
+    Result.Free;
+    raise;
+  end;
+end;
+
+procedure SaveBitmapsToAnimatedGIF(const Bitmaps: array of TBitmap; const FileName: string);
+var
+  GIF: TGIFImage;
+  Frame: TGIFFrame;
+begin
+  if Length(Bitmaps) = 0 then Exit;
+
+  GIF := TGIFImage.Create;
+  try
+    // GIFのベースとなるサイズを1枚目の画像に合わせる
+    GIF.Width := Bitmaps[0].Width;
+    GIF.Height := Bitmaps[0].Height;
+
+    Frame := nil;
+    for var i := Low(Bitmaps) to High(Bitmaps) do
+      Frame := GIF.Add(Bitmaps[i]);
+
+    // アニメーションのループ設定（0 = 無限ループ）
+    // ※Netscape拡張ブロックを追加してループ回数を指定します
+    TGIFAppExtNSLoop.Create(Frame).Loops:=0;
+    TGIFGraphicControlExtension.Create(Frame).Delay:=100;
+
+    // GIFファイルとして書き出し
+    GIF.SaveToFile(FileName);
+  finally
+    GIF.Free;
+  end;
+end;
+
+procedure TForm1.FormDestroy(Sender: TObject);
+begin
+  List.Free;
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  List:=TObjectList<TBitmap>.Create;
+end;
+
+procedure TForm1.Action1Execute(Sender: TObject);
+begin
+  Caption:=Format(title,['[録画中]']);
+  List.Clear;
+  Timer1.Enabled:=true;
+end;
+
+procedure TForm1.Action2Execute(Sender: TObject);
+begin
+  Close;
+end;
+
+procedure TForm1.FormCloseQuery(Sender: TObject; var CanClose: Boolean);
+begin
+  if Assigned(task) then
+  begin
+    CanClose:=false;
+    Showmessage('動作中です');
+  end;
+end;
+
+procedure TForm1.Timer1Timer(Sender: TObject);
+begin
+  if List.Count < 10 then
+    List.Add(CaptureScreen)
+  else
+  begin
+    Caption:=Format(title,['[処理中]']);
+    Timer1.Enabled:=false;
+    task:=TTask.Run(
+      procedure
+      begin
+        try
+          SaveBitmapsToAnimatedGIF(List.ToArray,'capture.gif');
+        finally
+          TThread.Queue(nil,
+            procedure
+            begin
+              task:=nil;
+              Showmessage('完成');
+              Caption:=Format(title,['']);
+            end);
+        end;
+      end);
+  end;
+end;
+
+end.
